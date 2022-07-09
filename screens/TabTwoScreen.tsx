130048b97c79bdd251bba24284e0fc1d67e9f100
Uchchash346 <mru416@gmail.com>
2022-07-09 22:53:43 +0600

    Implement redux

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -3,12 +3,16 @@ import { useSelector } from 'react-redux';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { selectCount } from '../store/counterSlice';
+import { selectTheme } from '../store/themeSlice';
 
 export default function TabTwoScreen() {
   const count = useSelector(selectCount);
+  const mode = useSelector(selectTheme);
+  const bgColor = mode === 'light' ? 'white' : 'black';
+  const textColor = mode === 'light' ? 'black' : 'white';
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>{count}</Text>
+    <View style={[styles.container, { backgroundColor: bgColor }]}>
+      <Text style={[styles.title, { color: textColor }]}>{count}</Text>
     </View>
   );
 }
